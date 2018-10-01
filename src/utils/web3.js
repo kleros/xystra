bbec1ac9e1deebdaa68c44a850a6c8f6fae3801a
epiqueras <epiquerass@gmail.com>
2018-09-30 20:14:26 -0500

    refactor: env var name

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
 const fetch = require('isomorphic-fetch')
 
 module.exports.web3 = new Web3(
-  new Web3.providers.HttpProvider(process.env.ETHEREUM_PROVIDER)
+  new Web3.providers.HttpProvider(process.env.ETHEREUM_HTTP_PROVIDER_URL)
 )
 
 module.exports.fetchList = async method => {
