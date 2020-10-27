4b61ef8d39657c5a4668e8b7bef07c811c1ebdd7
Ferit Tunçer <ferit@cryptolab.net>
2020-10-27 14:28:26 +0300

    feat(arbitrable-token-list): calculate removals

--- src/scrapers/arbitrable-token-list.js
+++ src/scrapers/arbitrable-token-list.js
@@ -28,9 +28,10 @@ module.exports = async (address, fromDate, toDate) => {
   )
 
   console.log(
-    `Calculating from ${fromDateBlockPair.block} (${fromDateBlockPair.date}) to ${toDateBlockPair.block} (${toDateBlockPair.date}).`
+    `Calculating from block ${fromDateBlockPair.block} (${fromDateBlockPair.date}) to block ${toDateBlockPair.block} (${toDateBlockPair.date}).`
   )
 
+  // REGISTRATION REQUESTS //
   const events = (await arbitrableTokenList.getPastEvents('TokenStatusChange', {
     fromBlock: fromDateBlockPair.block,
     toBlock: toDateBlockPair.block
@@ -38,23 +39,22 @@ module.exports = async (address, fromDate, toDate) => {
   console.log(
     `${events.length} TokenStatusChange events that request registration fetched, in given time frame.`
   )
-
   const distinctTokens = Object.keys(
     _.groupBy(events.map(e => e.returnValues._tokenID))
   )
-  console.log(`These events contain ${distinctTokens.length} distinct tokens.`)
-
+  console.log(
+    `These registration request events contain ${distinctTokens.length} distinct tokens.`
+  )
   const tokenInfos = await Promise.all(
     distinctTokens.map(t => arbitrableTokenList.methods.getTokenInfo(t).call())
   )
 
   const registeredTokens = tokenInfos.filter(token => token.status == '1')
-  console.log(`${registeredTokens.length} of them are registered.`)
+  console.log(
+    `In the given time frame ${registeredTokens.length} tokens became registered.`
+  )
   // console.log(registeredTokens.map(r => r.name).sort());
 
-  console.log(
-    '---------------------------------------------------------------------'
-  )
   const tokenSubmittedEvents = await arbitrableTokenList.getPastEvents(
     'TokenSubmitted',
     {
@@ -90,15 +90,42 @@ module.exports = async (address, fromDate, toDate) => {
 
   const fromAddresses = txs.map(tx => tx.from)
 
-  assert.strictEqual(
-    fromAddresses.length,
-    registeredTokens.length,
-    `Number of registered tokens ${registeredTokens.length} doesn't match number of eligible transactions ${fromAddresses.length}.`
-  )
   const countPerAddress = _.countBy(fromAddresses)
   const sortedCountPerAddress = Object.entries(countPerAddress).sort(
     (a, b) => b[1] - a[1]
   )
 
+  console.log(
+    'Here is the number of successful new token submission for each requester:'
+  )
   console.log(sortedCountPerAddress)
+
+  // CLEARING REQUESTS //
+  const removalEvents = (await arbitrableTokenList.getPastEvents(
+    'TokenStatusChange',
+    {
+      fromBlock: fromDateBlockPair.block,
+      toBlock: toDateBlockPair.block
+    }
+  )).filter(events => events.returnValues._status == '3')
+  console.log(
+    `${removalEvents.length} TokenStatusChange events that request clearing fetched, in given time frame.`
+  )
+
+  const removalDistinctTokens = Object.keys(
+    _.groupBy(removalEvents.map(e => e.returnValues._tokenID))
+  )
+  console.log(
+    `These clearing request events contain ${removalDistinctTokens.length} distinct tokens.`
+  )
+
+  const removalTokenInfos = await Promise.all(
+    removalDistinctTokens.map(t =>
+      arbitrableTokenList.methods.getTokenInfo(t).call()
+    )
+  )
+
+  const removedTokens = removalTokenInfos.filter(token => token.status == '0')
+  console.log(`${removedTokens.length} of them are removed.`)
+  console.log(removedTokens.map(r => r.name).sort())
 }
